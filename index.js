e09c7c415a4dccdddc4d40491981b53c9f4a22db
KazooTTT <yjwang.echo@gmail.com>
2023-05-07 22:21:23 +0800

    feat: script init

---
+++ index.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         github-form-control-selectable
+// @namespace    https://github.com/KazooTTT/github-form-control-selectable
+// @version      0.1
+// @description  enable user to select formControl element
+// @author       KazooTTT
+// @match        https://*.githb.com/*
+// @icon         https://github.com/kazoottt.png
+// @license      MIT
+// @grant        none
+// ==/UserScript==
+
+const insertStylesBySelector = (selector) => {
+  const style = document.createElement("style");
+  style.textContent = `
+  ${selector} {
+    user-select:auto
+  }
+  `;
+  const head = document.querySelector("head");
+  head.append(style);
+};
+
+(function () {
+  "use strict";
+  insertStylesBySelector(`.FormControl-label`);
+})();
